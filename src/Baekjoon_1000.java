4b9084d2fb357688ce48532e9aa8b62aa8b7228b
jeonjarae <sol4071@naver.com>
2026-02-25 20:19:23 +0900

    백준 1000번 문제 해결

---
+++ src/Baekjoon_1000.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Baekjoon_1000{
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        int A=input.nextInt();
+        int B=input.nextInt();
+        System.out.print(A+B);
+        input.close();
+    }
+}
