d558a74b821cb51003e78be92d770c843ea81755
kimdohong <rlaehghd12@daum.net>
2026-02-26 12:20:51 +0900

    프로그래머스 사칙연산 풀이

--- src/programmers_01.java
+++ src/programmers_01.java
@@ -1,7 +1,7 @@
 public class programmers_01 {
     // 자바 프로그램의 실행 진입점 (이게 있어야 실행 버튼이 생깁니다!)
     public static void main(String[] args) {
-        Solution sol = new Solution();
+        Solution1 sol = new Solution1();
 
         // 여기에 테스트하고 싶은 숫자를 넣어보세요 (예: 10, 20)
         int result = sol.solution(10, 20);
@@ -12,7 +12,7 @@ public class programmers_01 {
 }
 
 // 프로그래머스에서 복사해 온 코드
-class Solution {
+class Solution1 {
     public int solution(int num1, int num2) {
         int answer = -1;
         answer = num1 + num2;
