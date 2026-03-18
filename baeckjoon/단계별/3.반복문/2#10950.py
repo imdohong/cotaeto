fc21509fb8af37cc0425ff71fd8349dab3c60101
munjun0608 <munjun0608@gmail.com>
2026-03-18 15:28:39 +0900

    반복문 5문제 풀이

---
+++ baeckjoon/단계별/3.반복문/2#10950.py
@@ -0,0 +1,10 @@
+test_case = int(input())
+#result = []
+
+for i in range(test_case):
+    a, b = map(int, input().split())
+    print(a + b)
+'''result.append(a + b)
+
+for i in range(test_case):
+    print(result[i])   ''' 
---
+++ baeckjoon/단계별/3.반복문/5#25314.py
@@ -0,0 +1,7 @@
+n = int(input())
+sum = ''
+
+for i in range(1, n//4 +1):
+    sum = sum + 'long '
+
+print(sum + 'int')
