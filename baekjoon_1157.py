920a8ea65d95ac74938fb269543b4aa3d173c133
borjoy27 <borjoy27@gmail.com>
2026-03-31 14:46:29 +0900

    1157

--- baekjoon_1157.py
+++ baekjoon_1157.py
@@ -1,24 +1,33 @@
 word=input()
 word_upper=word.upper()
 
-# 입력받은 문자열이 이루어진 알파벳을 알기 위한 집합 word_set
-word_set=set(word_upper)
+"""이루어진 알파벳을 알기 위해
+중복되는 요소를 제거하는 set으로 감싼 뒤 
+순서쌍을 이용하기 위해 리스트로 변경한다.
+""" 
+word_list=list(set(word_upper))
 
-# (알파벳, 사용된 횟수) 순서쌍
+# (사용된 횟수, 알파벳) 순서쌍을 저장하기 위한 리스트
 count_list=list()
-for i in word_set:
-    # 알파벳 리스트
+for i in word_list:
+    # (사용된 횟수, 알파벳) 순서쌍을 저장
     count_list.append((word_upper.count(i), i))
 
-print(count_list)
+# 알파벳이 사용된 횟수 최대값 찾기
+max_count=0
+for count, alphabet in count_list:
+    if max_count<count:
+        max_count=count
 
-max=0
-for i in range(1,len(count_list)):
-    if count_list[i-1][0] <= count_list[i][0]:
-        max=count_list[i][0]
+# 찾은 최대값과 count가 같으면 max_alphas 리스트에 해당 alphabet을 저장
+max_alphas=list()
+for count, alphabet in count_list:
+    if max_count==count:
+        max_alphas.append(alphabet)
 
-for i in range(len(count_list)):
-    if count_list.count(max)>2 :
-        print("?")
-    
-print(max)
+# max_alphas 리스트에 저장된 알파벳이 2개 이상이면 ? 출력
+if len(max_alphas)>1:
+    print("?")      
+# 아니라면 알파벳 출력
+else:
+    print(max_alphas.pop())
